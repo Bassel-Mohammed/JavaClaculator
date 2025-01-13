fea5c00b5f039df5c62a85c0073c2397ba54229c
Bassel-Mohammed <bassel.alramoni@gmail.com>
2025-01-13 15:44:37 +0300

    created the project structure

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+
+public class Main {
+
+
+    // summation method
+    public static int summation(int x , int y){
+        return x + y;
+    }
+
+
+    public static void main (String []args){
+
+        int x = 5;
+        int y = 2;
+
+        System.out.println(summation(x , y));
+
+
+
+    }
+
+}
