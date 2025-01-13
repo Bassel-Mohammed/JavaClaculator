e48cb7e4f8d6f1cb4cd3bc4621584c99cb2ad901
Ahmed <bassel11ra@gmail.com>
2025-01-13 17:06:49 +0300

    removed --git-- word

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
     public static int multiplication(int x , int y){
         return x * y;
-    }git 
+    }
 
     public static int modulus(int x , int y){
         return x % y;
